5c95d76fda533b42564f9b25dcda0b8aa8676d07
Rupert Dance <rupertd@schwartzmedia.com.au>
2018-01-31 17:59:19 +1100

    done build locally

--- .cache/layouts/index.js
+++ .cache/layouts/index.js
@@ -1,7 +1,7 @@
 
   import React from "react"
-  import Component from "/Users/rupertdance/Code/contentful-testing/using-contentful/src/layouts/index.js"
-  import data from "/Users/rupertdance/Code/contentful-testing/using-contentful/.cache/json/layout-index.json"
+  import Component from "/Users/rupertdance/Code/contentful-testing/gatsby-rupert-testing/src/layouts/index.js"
+  import data from "/Users/rupertdance/Code/contentful-testing/gatsby-rupert-testing/.cache/json/layout-index.json"
 
   export default (props) => <Component {...props} {...data} />
   
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -3,34 +3,31 @@ const preferDefault = m => m && m.default || m
 
 
 exports.layouts = {
-  "layout---index": preferDefault(require("/Users/rupertdance/Code/contentful-testing/using-contentful/.cache/layouts/index.js"))
+  "layout---index": preferDefault(require("/Users/rupertdance/Code/contentful-testing/gatsby-rupert-testing/.cache/layouts/index.js"))
 }
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/rupertdance/Code/contentful-testing/using-contentful/.cache/dev-404-page.js")),
-  "component---src-templates-product-js": preferDefault(require("/Users/rupertdance/Code/contentful-testing/using-contentful/src/templates/product.js")),
-  "component---src-templates-category-js": preferDefault(require("/Users/rupertdance/Code/contentful-testing/using-contentful/src/templates/category.js")),
-  "component---src-pages-image-api-js": preferDefault(require("/Users/rupertdance/Code/contentful-testing/using-contentful/src/pages/image-api.js")),
-  "component---src-pages-index-js": preferDefault(require("/Users/rupertdance/Code/contentful-testing/using-contentful/src/pages/index.js"))
+  "component---src-templates-product-js": preferDefault(require("/Users/rupertdance/Code/contentful-testing/gatsby-rupert-testing/src/templates/product.js")),
+  "component---src-templates-category-js": preferDefault(require("/Users/rupertdance/Code/contentful-testing/gatsby-rupert-testing/src/templates/category.js")),
+  "component---src-pages-image-api-js": preferDefault(require("/Users/rupertdance/Code/contentful-testing/gatsby-rupert-testing/src/pages/image-api.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/rupertdance/Code/contentful-testing/gatsby-rupert-testing/src/pages/index.js"))
 }
 
 exports.json = {
-  "layout-index.json": require("/Users/rupertdance/Code/contentful-testing/using-contentful/.cache/json/layout-index.json"),
-  "dev-404-page.json": require("/Users/rupertdance/Code/contentful-testing/using-contentful/.cache/json/dev-404-page.json"),
-  "layout-index.json": require("/Users/rupertdance/Code/contentful-testing/using-contentful/.cache/json/layout-index.json"),
-  "products-c-5-ks-db-wse-xy-6-qeguc-y-aoac-s.json": require("/Users/rupertdance/Code/contentful-testing/using-contentful/.cache/json/products-c-5-ks-db-wse-xy-6-qeguc-y-aoac-s.json"),
-  "layout-index.json": require("/Users/rupertdance/Code/contentful-testing/using-contentful/.cache/json/layout-index.json"),
-  "products-c-6-dbj-wq-nd-9-sqccegcq-yq-224.json": require("/Users/rupertdance/Code/contentful-testing/using-contentful/.cache/json/products-c-6-dbj-wq-nd-9-sqccegcq-yq-224.json"),
-  "layout-index.json": require("/Users/rupertdance/Code/contentful-testing/using-contentful/.cache/json/layout-index.json"),
-  "products-c-3-d-vq-i-yj-4-d-owwc-ku-6-sgq-ogg.json": require("/Users/rupertdance/Code/contentful-testing/using-contentful/.cache/json/products-c-3-d-vq-i-yj-4-d-owwc-ku-6-sgq-ogg.json"),
-  "layout-index.json": require("/Users/rupertdance/Code/contentful-testing/using-contentful/.cache/json/layout-index.json"),
-  "products-c-4-bqrajv-a-8-e-6-qwgkieoqmq-o.json": require("/Users/rupertdance/Code/contentful-testing/using-contentful/.cache/json/products-c-4-bqrajv-a-8-e-6-qwgkieoqmq-o.json"),
-  "layout-index.json": require("/Users/rupertdance/Code/contentful-testing/using-contentful/.cache/json/layout-index.json"),
-  "categories-c-7-l-an-cobuu-wy-sqks-6-w-aw-y-2-a.json": require("/Users/rupertdance/Code/contentful-testing/using-contentful/.cache/json/categories-c-7-l-an-cobuu-wy-sqks-6-w-aw-y-2-a.json"),
-  "layout-index.json": require("/Users/rupertdance/Code/contentful-testing/using-contentful/.cache/json/layout-index.json"),
-  "categories-c-24-dpgb-de-g-ea-yy-8-ms-4-y-8-qmq.json": require("/Users/rupertdance/Code/contentful-testing/using-contentful/.cache/json/categories-c-24-dpgb-de-g-ea-yy-8-ms-4-y-8-qmq.json"),
-  "layout-index.json": require("/Users/rupertdance/Code/contentful-testing/using-contentful/.cache/json/layout-index.json"),
-  "image-api.json": require("/Users/rupertdance/Code/contentful-testing/using-contentful/.cache/json/image-api.json"),
-  "layout-index.json": require("/Users/rupertdance/Code/contentful-testing/using-contentful/.cache/json/layout-index.json"),
-  "index.json": require("/Users/rupertdance/Code/contentful-testing/using-contentful/.cache/json/index.json")
+  "layout-index.json": require("/Users/rupertdance/Code/contentful-testing/gatsby-rupert-testing/.cache/json/layout-index.json"),
+  "products-c-5-ks-db-wse-xy-6-qeguc-y-aoac-s.json": require("/Users/rupertdance/Code/contentful-testing/gatsby-rupert-testing/.cache/json/products-c-5-ks-db-wse-xy-6-qeguc-y-aoac-s.json"),
+  "layout-index.json": require("/Users/rupertdance/Code/contentful-testing/gatsby-rupert-testing/.cache/json/layout-index.json"),
+  "products-c-6-dbj-wq-nd-9-sqccegcq-yq-224.json": require("/Users/rupertdance/Code/contentful-testing/gatsby-rupert-testing/.cache/json/products-c-6-dbj-wq-nd-9-sqccegcq-yq-224.json"),
+  "layout-index.json": require("/Users/rupertdance/Code/contentful-testing/gatsby-rupert-testing/.cache/json/layout-index.json"),
+  "products-c-3-d-vq-i-yj-4-d-owwc-ku-6-sgq-ogg.json": require("/Users/rupertdance/Code/contentful-testing/gatsby-rupert-testing/.cache/json/products-c-3-d-vq-i-yj-4-d-owwc-ku-6-sgq-ogg.json"),
+  "layout-index.json": require("/Users/rupertdance/Code/contentful-testing/gatsby-rupert-testing/.cache/json/layout-index.json"),
+  "products-c-4-bqrajv-a-8-e-6-qwgkieoqmq-o.json": require("/Users/rupertdance/Code/contentful-testing/gatsby-rupert-testing/.cache/json/products-c-4-bqrajv-a-8-e-6-qwgkieoqmq-o.json"),
+  "layout-index.json": require("/Users/rupertdance/Code/contentful-testing/gatsby-rupert-testing/.cache/json/layout-index.json"),
+  "categories-c-7-l-an-cobuu-wy-sqks-6-w-aw-y-2-a.json": require("/Users/rupertdance/Code/contentful-testing/gatsby-rupert-testing/.cache/json/categories-c-7-l-an-cobuu-wy-sqks-6-w-aw-y-2-a.json"),
+  "layout-index.json": require("/Users/rupertdance/Code/contentful-testing/gatsby-rupert-testing/.cache/json/layout-index.json"),
+  "categories-c-24-dpgb-de-g-ea-yy-8-ms-4-y-8-qmq.json": require("/Users/rupertdance/Code/contentful-testing/gatsby-rupert-testing/.cache/json/categories-c-24-dpgb-de-g-ea-yy-8-ms-4-y-8-qmq.json"),
+  "layout-index.json": require("/Users/rupertdance/Code/contentful-testing/gatsby-rupert-testing/.cache/json/layout-index.json"),
+  "image-api.json": require("/Users/rupertdance/Code/contentful-testing/gatsby-rupert-testing/.cache/json/image-api.json"),
+  "layout-index.json": require("/Users/rupertdance/Code/contentful-testing/gatsby-rupert-testing/.cache/json/layout-index.json"),
+  "index.json": require("/Users/rupertdance/Code/contentful-testing/gatsby-rupert-testing/.cache/json/index.json")
 }
